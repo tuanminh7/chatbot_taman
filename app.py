e9b7fdab358c309681b34e17fe0829be11dae2de
Tuan Minh <tuanminh07@gmail.com>
2025-08-14 23:55:03 +0700

    Remove app.run and switch to gunicorn

--- app.py
+++ app.py
@@ -865,7 +865,3 @@ def get_questions():
     selected = random.sample(questions, min(10, len(questions)))
     return jsonify(selected)
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
